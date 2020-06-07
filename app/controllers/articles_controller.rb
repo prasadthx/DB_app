aab7e5662c25614655d78ad3fefefe6b488b56bf
Prasad Maruti Zore <prasadzore31@gmail.com>
2020-06-08 00:33:34 +0530

    DB COMPLETE

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+  before_action :set_article, only: [:edit , :show, :update, :destroy]
   def index
     @articles=Article.all
   end
@@ -19,15 +19,15 @@ class ArticlesController < ApplicationController
   end
 
   def show()
-    @article=Article.find(params[:id])
+
   end
 
   def edit()
-    @article=Article.find(params[:id])
+
   end
 
   def update()
-    @article=Article.find(params[:id])
+
     if @article.update(article_params)
       flash[:notice] = "Article was updated successfully"
       redirect_to article_path(@article)
@@ -36,9 +36,20 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy()
+
+    @article.destroy
+    flash[:notice]="Article was successfully deleted"
+    redirect_to articles_path
+  end
+
   private
-    def article_params
-      return params.require(:article).permit( :title , :description)
-    end
+
+  def set_article
+    @article=Article.find(params[:id])
+  end
+  def article_params
+    return params.require(:article).permit( :title , :description)
+  end
 
 end
