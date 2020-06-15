44fbebda739379f7ef9add1b18e93944c18a912b
Prasad Maruti Zore <prasadzore31@gmail.com>
2020-06-15 12:25:23 +0530

    C

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,8 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article=Article.new(article_params)
+    @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article was created successfully."
       redirect_to article_path(@article)
